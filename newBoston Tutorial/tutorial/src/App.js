b27a6fc14194aee8df835b055dcb3cf3ce36ea8f
Abhishek Shrinivasan <abhishek.shrinivasan@gmail.com>
2018-01-19 18:42:48 -0500

    newBoston tutorial app

--- newBoston Tutorial/tutorial/src/App.js
+++ newBoston Tutorial/tutorial/src/App.js
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
-class Comment extends Component{
-
-    render(){
-        let title,genre;
-        if(this.props.title){
-          title='Title: '+this.props.title;
-        }
-        if(this.props.genre){
-            genre='Genre: '+this.props.genre;
-        }
-
-        return(
-            <div>
-                <h1>{title}</h1>
-                <h2>{genre}</h2>
-            </div>
-        );
-    }
-}
+import './main.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-         <Comment/>
+      <div>
+          React Tutorial
       </div>
     );
   }
